7e89a6c3c8e090deb996011fc96bb5e07edc4d13
kananxaliloff <kananxaliloff@gmail.com>
2026-04-26 14:08:43 +0400

    feat: add auth controllers and routes

---
+++ backend/controllers/auth.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"finance-manager-backend/database"
+	"finance-manager-backend/models"
+	"finance-manager-backend/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type RegisterUserRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginUserRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+func Register(c *gin.Context) {
+	var req RegisterUserRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+	database.DB.Where("email = ?", req.Email).First(&user)
+	if user.ID != 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		return
+	}
+
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	user = models.User{
+		Email:        req.Email,
+		PasswordHash: hashedPassword,
+	}
+
+	database.DB.Create(&user)
+	c.JSON(http.StatusOK, gin.H{"userId": user.ID, "message": "User registered successfully"})
+}
+
+func Login(c *gin.Context) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+	database.DB.Where("email = ?", req.Email).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	if !utils.VerifyPassword(req.Password, user.PasswordHash) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"os"
 
+	"finance-manager-backend/controllers"
 	"finance-manager-backend/database"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -25,10 +27,14 @@ func main() {
 	// Basic route
 	r.GET("/api/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"status": "ok",
+			"status":  "ok",
 			"message": "Finance Manager API is running",
 		})
 	})
+
+	auth := r.Group("/api/v1/auth")
+	auth.POST("/register", controllers.Register)
+	auth.POST("/login", controllers.Login)
 
 	port := os.Getenv("PORT")
 	if port == "" {
